7c0aee8a2c746d77e6ed6bdcb2decdc06e990e14
sionsideup <sionchung@hotmail.com>
2016-03-08 19:00:11 -0500

    getting request

--- index.js
+++ index.js
@@ -43,23 +43,32 @@ if(window.location.hash) {
     window.location.href = "https://api.instagram.com/oauth/authorize/?client_id=2d63a3847c6740b3be538b860ab6d534&redirect_uri=http://sionsideup.github.io/instagramproj&response_type=token";
 }
 
-
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
+$.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: 'https://api.instagram.com/v1/locations/search?lat='+pos.lat+'&lng='+pos.lng+'&access_token='+token,
+    success: function(data) {
+        console.log(data);
         }
+});
 
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
-    }
-}
-
-aClient = new HttpClient();
-aClient.get('https://api.instagram.com/v1/locations/search?lat='+pos.lat+'&lng='+pos.lng+'&access_token='+token), 
-    function(response) {
-    console.log(response);
-};
+//var HttpClient = function() {
+//    this.get = function(aUrl, aCallback) {
+//        var anHttpRequest = new XMLHttpRequest();
+//        anHttpRequest.onreadystatechange = function() { 
+//            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+//                aCallback(anHttpRequest.responseText);
+//        }
+//
+//        anHttpRequest.open( "GET", aUrl, true );            
+//        anHttpRequest.send( null );
+//    }
+//}
+//
+//aClient = new HttpClient();
+//aClient.get('https://api.instagram.com/v1/locations/search?lat='+pos.lat+'&lng='+pos.lng+'&access_token='+token), 
+//    function(response) {
+//    console.log(response);
+//};
 ////http://stackoverflow.com/questions/247483/http-get-request-in-javascript
