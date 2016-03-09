4bb3eb7bffaf37ff6b13eadaa0ed52ca58b58309
sionsideup <sionchung@hotmail.com>
2016-03-09 12:57:09 -0500

    a

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ if (navigator.geolocation) {
                             type: "GET",
                             dataType: "jsonp",
                             cache: false,
-                            url: 'https://api.instagram.com/v1/locations/'+ data.data[i].id +'/media/recent?access_token='+token,
+                            url: 'https://api.instagram.com/v1/locations/'+ data.data[i].id +'/media/recent?'+token,
                             success: function(data) {
                                 // in here you want to add these photos to the marker (for loop)
                                 console.log(data);
