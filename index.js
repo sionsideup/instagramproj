4172a1012b09cd63a254ea688b1cf87156854d20
sionsideup <sionchung@hotmail.com>
2016-03-09 03:09:42 -0500

    hljhl

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
+        zoom: 15,
         center: {lat: -33.9, lng: 151.2}
     });
     var infoWindow = new google.maps.InfoWindow({map: map});
@@ -21,7 +21,7 @@ function initMap() {
     //            console.log(token);
                 token = token.substring(1,token.length);
     //            console.log(token);
-                var places = $.ajax({
+                var places = return $.ajax({
                     type: "GET",
                     dataType: "jsonp",
                     cache: false,
