0265d9df7ff797607eddb498f441b062ac8bdb85
sionsideup <sionchung@hotmail.com>
2016-03-09 00:35:27 -0500

    adding flags

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function initMap() {
+//function initMap() {
+//  var map = new google.maps.Map(document.getElementById('map'), {
+//    center: {lat: -34.397, lng: 150.644},
+//    zoom: 15,
+//  });
+    
+    function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 15
+    zoom: 10,
+    center: {lat: -33.9, lng: 151.2}
   });
+
+  setMarkers(map);
+} 
+    
   var infoWindow = new google.maps.InfoWindow({map: map});
 
   // Try HTML5 geolocation.
@@ -29,9 +39,45 @@ function initMap() {
             url: 'https://api.instagram.com/v1/locations/search?lat='+pos.lat+'&lng='+pos.lng+'&'+token,
             success: function(data) {
                 console.log(data);
-                for (i in data.data.length){
+                for (var i = 0; i < data.data.length; i++){
                     console.log(data.data[i]);
-                    // here you want to add a marker for each location you find 
+                    
+                    function setMarkers(map) {
+                          // Adds markers to the map.
+
+                          // Marker sizes are expressed as a Size of X,Y where the origin of the image
+                          // (0,0) is located in the top left of the image.
+
+                          // Origins, anchor positions and coordinates of the marker increase in the X
+                          // direction to the right and in the Y direction down.
+                          var image = {
+                            url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                            // This marker is 20 pixels wide by 32 pixels high.
+                            size: new google.maps.Size(20, 32),
+                            // The origin for this image is (0, 0).
+                            origin: new google.maps.Point(0, 0),
+                            // The anchor for this image is the base of the flagpole at (0, 32).
+                            anchor: new google.maps.Point(0, 32)
+                          };
+                          // Shapes define the clickable region of the icon. The type defines an HTML
+                          // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+                          // The final coordinate closes the poly by connecting to the first coordinate.
+                          var shape = {
+                            coords: [1, 1, 1, 20, 18, 20, 18, 1],
+                            type: 'poly'
+                          };
+                          for (var i = 0; i < data.data.length; i++) {
+                            var marker = new google.maps.Marker({
+                              position: {lat: data.data[i].latitude, lng: data.data[i].longitude},
+                              map: map,
+                              icon: image,
+                              shape: shape,
+                              title: data.data[i].name, --------------------?
+                              zIndex: i
+                            });
+                          }
+                        }
+                    
                     // use data.data[i].latitude, data.data[i].longitude to get position to add each marker, you can also get the name values here incase you want to add a location to each marker so that the user will know what the location is for the marker
                     $.ajax({
                         type: "GET",
