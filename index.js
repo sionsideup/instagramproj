d5a41c95a6506a7012487e849af08747fca903cf
sionsideup <sionchung@hotmail.com>
2016-03-09 01:10:19 -0500

    fixing bracket

--- index.js
+++ index.js
@@ -90,8 +90,9 @@
                     });
                 }
             })            
-        };
-    } else {
+      };
+    })
+  } else {
     window.location.href = "https://api.instagram.com/oauth/authorize/?client_id=2d63a3847c6740b3be538b860ab6d534&redirect_uri=http://sionsideup.github.io/instagramproj&response_type=token&scope=public_content";
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
