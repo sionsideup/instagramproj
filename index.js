efe447433f61acfeddb80e2f14da3e758f553337
sionsideup <sionchung@hotmail.com>
2016-03-09 02:53:30 -0500

    omgwtfbbq

--- index.js
+++ index.js
@@ -22,60 +22,26 @@ function initMap() {
     //            console.log(token);
                 token = token.substring(1,token.length);
     //            console.log(token);
-                $.ajax({
+                var places = $.ajax({
                     type: "GET",
                     dataType: "jsonp",
                     cache: false,
                     url: 'https://api.instagram.com/v1/locations/search?lat='+pos.lat+'&lng='+pos.lng+'&'+token,
                     success: function(data) {
-    //                    console.log(data);
-                        for (var i = 0; i < data.data.length; i++){
-    //                        console.log(data.data[i]);
-                            function setMarkers(map) {
-                                var image = {
-                                    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-                                    size: new google.maps.Size(20, 32),
-                                    origin: new google.maps.Point(0, 0),
-                                    anchor: new google.maps.Point(0, 32)
-                                };
-                                var shape = {
-                                    coords: [1, 1, 1, 20, 18, 20, 18, 1],
-                                    type: 'poly'
-                                };
-                                var marker = new google.maps.Marker({
-                                    position: {lat: data.data[i].latitude, lng: data.data[i].longitude},
-                                    map: map,
-                                    icon: image,
-                                    shape: shape,
-                                    title: data.data[i].name,
-                                    zIndex: i
-                                });
-                                var pictureTemplate = <
-                                var picWindow = new google.maps.InfoWindow({content:pictureTemplate});
-                                marker.addListener('click',function(){
-                                   picWindow.open(map,marker); 
-                                });
-                            }
-                            setMarkers(map);
-    //                        console.log("test");
-    //                        console.log(data);
-    //                        console.log(data.data);
-    //                        console.log(i);
-                            console.log(data.data[i]);
-                            $.ajax({
-                                type: "GET",
-                                dataType: "jsonp",
-                                cache: false,
-                                url: 'https://api.instagram.com/v1/locations/'+ data.data[i].id +'/media/recent?access_token='+token,
-                                success: function(data) {
-                                    // in here you want to add these photos to the marker (for loop)
-    //                                console.log(data);
-                                }
-                            });
-                        }
+                        return data
                     }
-                });            
-          } else {
+                });
+                console.log(places);
+//                var pics = $.ajax({
+//                                type: "GET",
+//                                dataType: "jsonp",
+//                                cache: false,
+//                                url: 'https://api.instagram.com/v1/locations/'+ data.data[i].id +'/media/recent?access_token='+token,
+//                                success: function(data) {
+//                                    return data;
+//                                };
+//                            });
+            } else {
               window.location.href = "https://api.instagram.com/oauth/authorize/?client_id=2d63a3847c6740b3be538b860ab6d534&redirect_uri=http://sionsideup.github.io/instagramproj&response_type=token&scope=public_content";
           }
         });
